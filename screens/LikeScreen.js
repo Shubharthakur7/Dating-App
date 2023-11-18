2d23d77b47284ee107837d93f6670573c646ffbb
Shubham Singh <shubhamsingh@Shubhams-MacBook-Air.local>
2023-11-18 05:51:11 -0800

    initial commit

---
+++ screens/LikeScreen.js
@@ -0,0 +1,13 @@
+// HomeScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Like Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
