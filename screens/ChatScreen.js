43106aadf546cdef884c9eff0db374e5f6fcc15f
Shubham Singh <rshubhams12@gmail.com>
2024-02-18 21:45:18 +0530

    added explore page

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,13 +1,114 @@
 // ExploreScreen.js
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, ImageBackground, Image } from 'react-native';
 
-const ExploreScreen = () => {
+const ChatScreen = () => {
+  const [activeTab, setActiveTab] = useState('allChats');
+
+  // Sample data
+  const data = [
+    { id: 1, name: 'John', image: '../assets/images/image1.jpg', type: 'allChats' },
+    { id: 2, name: 'Jane', image: '../assets/images/2.jpg', type: 'allChats' },
+    { id: 3, name: 'Alice', image: '../assets/images/3.jpg', type: 'newMatches' },
+    { id: 4, name: 'Bob', image: '../assets/images/4.jpg', type: 'newMatches' },
+  ];
+
+  // Filtered data based on active tab
+  const filteredData = activeTab === 'allChats' ? data : data.filter(item => item.type === 'newMatches');
+
   return (
-    <View>
-      <Text>Chat Screen</Text>
+    <View style={styles.container}>
+      {/* Tabs */}
+      <View style={styles.tabContainer}>
+        <TouchableOpacity onPress={() => setActiveTab('allChats')} style={[styles.tab, activeTab === 'allChats' && styles.activeTab]}>
+          <Text style={[styles.tabText, activeTab === 'allChats' && styles.activeTabText]}>All Chats</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setActiveTab('newMatches')} style={[styles.tab, activeTab === 'newMatches' && styles.activeTab]}>
+          <Text style={[styles.tabText, activeTab === 'newMatches' && styles.activeTabText]}>New Matches</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Data */}
+      {filteredData.length === 0 ? (
+        <ImageBackground source={require('../assets/images/users/1.jpg')} style={styles.backgroundImage}>
+          <Text style={styles.noDataText}>Oops! No data found.</Text>
+        </ImageBackground>
+      ) : (
+        <FlatList
+          data={filteredData}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.itemContainer}>
+              <View style={styles.imageContainer}>
+                <Image source={{uri: item.image}} style={styles.profileImage} />
+              </View>
+              <Text>{item.name}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 };
 
-export default ExploreScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    marginTop: 30,
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: '#f0f0f0',
+  },
+  activeTab: {
+    backgroundColor: 'red',
+  },
+  tabText: {
+    fontSize: 16,
+  },
+  activeTabText: {
+    color: 'white',
+  },
+  noDataText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 18,
+    color: '#fff',
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    resizeMode: 'cover',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  imageContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    overflow: 'hidden',
+    borderColor: "red",
+    marginRight: 10,
+  },
+  profileImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+});
+
+
+export default ChatScreen;
--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,13 +1,65 @@
 // ExploreScreen.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet, Dimensions, Text, ScrollView, ImageBackground } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
 
 const ExploreScreen = () => {
   return (
-    <View>
-      <Text>Explore Screen</Text>
+    <ScrollView>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <ImageWithTitle source={require('../assets/images/image1.jpg')} title="Duggu" />
+        <View style={styles.imageGap} />
+        <ImageWithTitle source={require('../assets/images/image2.jpg')} title="Title 2" />
+      </View>
+      <View style={styles.row}>
+      <ImageWithTitle source={require('../assets/images/image3.jpg')} title="Title 3" />
+      <View style={styles.imageGap} />
+        <ImageWithTitle source={require('../assets/images/image4.jpg')} title="Darshu" />
+      </View>
+      <View style={styles.row}>
+      <ImageWithTitle source={require('../assets/images/image5.jpg')} title="Title 5" />
+      </View>
     </View>
+    </ScrollView>
   );
 };
 
+const ImageWithTitle = ({ source, title }) => {
+  return (
+    <ImageBackground source={source} style={styles.image} imageStyle={styles.imageStyle}>
+      <Text style={styles.title}>{title}</Text>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    width: '100%',
+    height: screenWidth * 1.5 * 0.5 + 10,
+    padding: 5,
+  },
+  imageStyle: {
+    borderRadius: 10,
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  imageGap: {
+    width: 5,
+  },
+});
+
 export default ExploreScreen;
+
